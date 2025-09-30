a8f6ffddcd466762d6b50c3fe986561acc4b46e6
joao paulo <joao.paulo.f.guimaraes@gmail.com>
2025-09-30 14:18:24 -0300

    dados unificando carga-horaria

---
+++ dados_2.py
@@ -0,0 +1,142 @@
+# Dados das cargas horárias por grupo
+
+# Dados base (tudo menos os integrados)
+b = [
+    {
+        "grupo": "EBM",
+        "media": 4.6
+    },
+    {
+        "grupo": "Jogos",
+        "media": 1.0
+    },
+    {
+        "grupo": "Multimedia",
+        "media": 0.0
+    },
+    {
+        "grupo": "Redes",
+        "media": 3.6
+    },
+    {
+        "grupo": "SI",
+        "media": 6.8
+    }, 
+    {
+        "grupo": "manutencao_eletro",
+        "media": 4.0
+    }
+]
+
+# Integrado de Informática
+info = [
+    {
+        "grupo": "EBM",
+        "media": 4.6
+    },
+    {
+        "grupo": "Jogos",
+        "media": 1.0
+    },
+    {
+        "grupo": "Multimedia",
+        "media": 0.0
+    },
+    {
+        "grupo": "Redes",
+        "media": 7.6
+    },
+    {
+        "grupo": "SI",
+        "media": 10.8
+    },
+{
+        "grupo": "manutencao_eletro",
+        "media": 5.6
+    }
+]
+
+# Integrado de EBM
+ebm = [
+    {
+        "grupo": "EBM",
+        "media": 8.9
+    },
+    {
+        "grupo": "Jogos",
+        "media": 1.0
+    },
+    {
+        "grupo": "Multimedia",
+        "media": 0.0
+    },
+    {
+        "grupo": "Redes",
+        "media": 3.6
+    },
+    {
+        "grupo": "SI",
+        "media": 6.8
+    },
+    {
+        "grupo": "manutencao_eletro",
+        "media": 6.2
+    }
+]
+
+# Integrado de Jogos
+jogos = [
+    {
+        "grupo": "EBM",
+        "media": 4.6
+    },
+    {
+        "grupo": "Jogos",
+        "media": 10.0
+    },
+    {
+        "grupo": "Multimedia",
+        "media": 12
+    },
+    {
+        "grupo": "Redes",
+        "media": 5.6
+    },
+    {
+        "grupo": "SI",
+        "media": 6.8
+    },
+    {
+        "grupo": "manutencao_eletro",
+        "media": 4.0
+    }
+]
+
+# Integrado de Eletro
+eletro = [
+    {
+        "grupo": "EBM",
+        "media": 4.6
+    },
+    {
+        "grupo": "Jogos",
+        "media": 1
+    },
+    {
+        "grupo": "Multimedia",
+        "media": 0
+    },
+    {
+        "grupo": "Redes",
+        "media": 3.6
+    },
+    {
+        "grupo": "SI",
+        "media": 6.8
+    },
+    {
+        "grupo": "manutencao_eletro",
+        "media": 9.4
+    }
+]
+
